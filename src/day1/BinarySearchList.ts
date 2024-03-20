f7b95a17adfa91cb50c907ca0f5e6e614532c5ba
Mudassir Nizamani <mudasiralinizamani@gmail.com>
2024-03-20 05:39:45 +0500

    Refactor binary search and crystal ball algorithms

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-  return false
+    let leftIndex = 0;
+    let rightIndex = haystack.length - 1;
+
+    while (leftIndex <= rightIndex) {
+        const middleIndex = Math.floor(
+            leftIndex + (rightIndex - leftIndex) / 2,
+        );
+        const middleValue = haystack[middleIndex];
+
+        if (middleValue === needle) {
+            return true;
+        } else if (middleValue > needle) {
+            rightIndex = middleIndex - 1;
+        } else {
+            leftIndex = middleIndex + 1;
+        }
+    }
+
+    return false;
 }
