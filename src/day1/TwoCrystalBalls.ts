6ba5301dfc5517690b34a2defecf05d0f96c248c
Mudassir Nizamani <mudasiralinizamani@gmail.com>
2024-03-28 21:04:32 +0500

    Refactor BubbleSort and TwoCrystalBalls functions

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -10,7 +10,7 @@ export default function two_crystal_balls(breaks: boolean[]): number {
 
     i -= jumpAmount;
 
-    for (let j = 0; j < jumpAmount && i < breaks.length; ++j, i++) {
+    for (let j = 0; j <= jumpAmount && i < breaks.length; ++j, i++) {
         if (breaks[i]) {
             return i;
         }
